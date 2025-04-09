4f1746dd05d1812facbbfe5676f931796005aceb
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-04-09 18:37:18 +0200

    Update : Change ID to Guild ID

--- commands/register.mjs
+++ commands/register.mjs
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 import { REST, Routes } from "discord.js";
 config();
 
-const guildId = process.env.APP_MODE === "production" ? process.env.PROD_SERVER_ID : process.env.TEST_SERVER_ID;
+const guildId = process.env.PROD_SERVER_ID;
 
 const commands = [
     {
