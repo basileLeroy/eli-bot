8d00739b35952558abc37982a9a5cbbbdb72cd96
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-04-11 11:39:30 +0200

    Create : Add new barrel file for command functions

---
+++ commands/index.mjs
@@ -0,0 +1,14 @@
+import handleAnnounce from "./announce/announce.mjs";
+import handlePing from "./ping/ping.mjs";
+import handleReport from "./report/report.mjs";
+import handleSuggest from "./suggest/suggest.mjs";
+import handleWhoami from "./whoami/whoami.mjs";
+
+
+export {
+    handleAnnounce,
+    handlePing,
+    handleReport,
+    handleSuggest,
+    handleWhoami
+}
